662fdb282859c55ad4d5f31e48bfb2712c82843c
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2023-03-16 23:41:18 -0300

    feat: dotenv port express

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const http = require('http');
 const express = require('express');
 const Socket = require('./Socket');
 
+require('dotenv').config()
 const app = express();
 const server = http.createServer(app);
 const socket = new Socket(server);
 
 app.use(express.static(__dirname + '/public'));
 
-server.listen(3000, () => {
+server.listen(process.env.PORT, () => {
     console.log(`Rádio AI is live!`);
 });
