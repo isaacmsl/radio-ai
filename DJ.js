e9ddfac98ceb5d1b6e9161ea28ba459979561b62
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2023-03-23 14:04:39 -0300

    feat: 3 jamendo songs on queue

--- DJ.js
+++ DJ.js
@@ -69,7 +69,7 @@ class DJ {
                 }
             });
             if (!this.jamendoQueue.isEmpty) {
-                this.queueJamendos(4);
+                this.queueJamendos(3);
             } else {
                 this.jamendoQueue.pull();
                 this.queue.enqueue(songs[Math.floor(Math.random() * songs.length)]);
